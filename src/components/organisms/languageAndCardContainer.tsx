240cd9d0a6494261014c06aac513c25d15332b94
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-08-06 23:34:36 +0900

    npx install expo-speech

--- src/components/organisms/languageAndCardContainer.tsx
+++ src/components/organisms/languageAndCardContainer.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Divider } from '@rneui/themed';
-import LanguageAndCard from '../molecules/languageAndCard';
-import { dimensions } from '../../constants/dimensions';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Divider } from "@rneui/themed";
+import LanguageAndCard from "../molecules/languageAndCard";
+import { dimensions } from "../../constants/dimensions";
+import * as Speech from "expo-speech";
+import { TouchableIcon } from "../atoms/touchableIcon";
+import { LoadImage } from "../../utils/loadImages";
 
 interface LanguageAndCardContainerProps {
   sourceLanguageName: string;
@@ -17,27 +20,53 @@ const LanguageAndCardContainer: React.FC<LanguageAndCardContainerProps> = ({
   targetLanguageName,
   targetText,
 }) => {
+  const speekText = (text: string, lan: string) => {
+    if (lan === "日本語") {
+      lan = "ja";
+    } else if (lan === "英語") {
+      lan = "en";
+    }
+    console.log("speaking", text, lan);
+    Speech.speak(text, {
+      language: lan,
+    })
+  };
+
   return (
     <View style={styles.cardContainer}>
-      <LanguageAndCard languageName={sourceLanguageName} textContent={sourceText} />
+      <LanguageAndCard
+        languageName={sourceLanguageName}
+        textContent={sourceText}
+      />
+      <TouchableIcon
+        imageSource={LoadImage.translateIcon}
+        onPress={() => speekText(sourceText, sourceLanguageName)}
+      />
       <View style={styles.separator} />
       {/* <Divider /> */}
-      <LanguageAndCard languageName={targetLanguageName} textContent={targetText} />
+      <LanguageAndCard
+        languageName={targetLanguageName}
+        textContent={targetText}
+      />
+      <TouchableIcon
+        imageSource={LoadImage.translateIcon}
+        onPress={() => speekText(targetText, targetLanguageName)}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
-    width: '90%',
-    alignItems: 'center',
+    width: "90%",
+    alignItems: "center",
   },
   separator: {
-    width: '80%',
+    width: "80%",
     height: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     marginVertical: dimensions.SCREEN_HEIGHT * 0.025,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
 });
 
