ee80c4f7764220fdc8a1033110ed069c9db0f992
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-08-07 15:33:09 +0900

    仮のデータでライブラリ画面の単語カードを作成

--- src/components/organisms/flashCardsFlip.tsx
+++ src/components/organisms/flashCardsFlip.tsx
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from "react";
 import { View, StyleSheet, Animated, TouchableOpacity } from "react-native";
 import FlashCard from "../molecules/flashCard";
+import Card from "../atoms/card";
+import { dimensions } from "../../constants/dimensions";
+import { colors } from "../../styles/colors";
 
 const FlashCardsFlip = () => {
   const flipAnimations = useRef(cards.map(() => new Animated.Value(0))).current;
@@ -41,10 +44,14 @@ const FlashCardsFlip = () => {
     transform: [{ rotateX: backInterpolate(animation) }],
   });
 
-  const renderCard = (text: string, animatedStyle: any) => {
+  const renderCard = (text: string, lan: string, animatedStyle: any) => {
     return (
       <Animated.View style={[animatedStyle]}>
-        <FlashCard text={text} />
+        <Card
+          textContent={text}
+          languageName={lan}
+          cardStyle={styles.cardContainer}
+        />
       </Animated.View>
     );
   };
@@ -52,16 +59,22 @@ const FlashCardsFlip = () => {
   return (
     <View style={styles.container}>
       {cards.map((card, index) => (
-        <TouchableOpacity
-          key={card.id}
-          onPress={() => flipCard(index)}
-          style={styles.cardContainer}
-        >
+        <TouchableOpacity key={card.id} onPress={() => flipCard(index)}>
           {flipped[index]
-            ? renderCard(card.back, backAnimatedStyle(flipAnimations[index]))
-            : renderCard(card.front, frontAnimatedStyle(flipAnimations[index]))}
+            ? renderCard(
+                card.back,
+                card.backLan,
+                backAnimatedStyle(flipAnimations[index])
+              )
+            : renderCard(
+                card.front,
+                card.frontLan,
+                frontAnimatedStyle(flipAnimations[index])
+              )}
         </TouchableOpacity>
       ))}
+      {/* 発音ボタンとダイアルボタンが被るのを防ぐためにスペースを追加 */}
+      <View style={styles.spaceBottom} />
     </View>
   );
 };
@@ -69,12 +82,20 @@ const FlashCardsFlip = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
   cardContainer: {
-    width: "90%",
+    width: dimensions.SCREEN_WIDTH * 0.8,
+    height: dimensions.SCREEN_HEIGHT * 0.15,
+    marginVertical: dimensions.SCREEN_HEIGHT * 0.015,
+    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: colors.backgroundPrimary,
+    borderRadius: 10,
+  },
+  spaceBottom: {
+    marginBottom: dimensions.SCREEN_HEIGHT * 0.1,
   },
 });
 
@@ -83,26 +104,36 @@ const cards = [
     id: 1,
     front: "front 1",
     back: "back 1",
+    frontLan: "en",
+    backLan: "ja",
   },
   {
     id: 2,
     front: "front 2",
     back: "back 2",
+    frontLan: "en",
+    backLan: "ja",
   },
   {
     id: 3,
     front: "front 3",
     back: "back 3",
+    frontLan: "en",
+    backLan: "ja",
   },
   {
     id: 4,
     front: "front 4",
     back: "back 4",
+    frontLan: "en",
+    backLan: "ja",
   },
   {
     id: 5,
     front: "front 5",
     back: "back 5",
+    frontLan: "en",
+    backLan: "ja",
   },
 ];
 
