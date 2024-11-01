206ac1ca9d8f189ddb8adb6868459307c530eac9
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-11-01 15:44:01 +0900

    サーバーサイドの作成

--- src/services/deeplService.ts
+++ src/services/deeplService.ts
@@ -1,31 +1,19 @@
 import axios from 'axios';
 
-interface TranslateResponse {
-  translations: { text: string }[];
-}
-
 const translateText = async (sourceLang: string, targetLang: string, text: string): Promise<string> => {
   try {
-    const response = await axios.post<TranslateResponse>('https://api-free.deepl.com/v2/translate', {
-      text: [text],
-      target_lang: targetLang,
-      source_lang: sourceLang
+    const response = await axios.post('https://deepl-translate-server.onrender.com/translate', {
+      sourceLang,
+      targetLang,
+      text,
     }, {
-      headers: {
-        'Authorization': 'DeepL-Auth-Key cdd55087-b217-4187-ab86-7df4ea8ddcc1:fx', 
-        'Content-Type': 'application/json'
-      },
       timeout: 5000,
     });
-    const translatedText = response.data.translations[0].text;
+    const translatedText = response.data.translatedText;
     return translatedText;
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log("Translation error:", error.response ? error.response.data : error.message);
-    } else {
-      console.log("Translation error:", error);
-    }
-    throw error;  // エラーを再スローして呼び出し元で処理できるようにする
+    console.error("Translation error:", error);
+    throw error;
   }
 };
 
